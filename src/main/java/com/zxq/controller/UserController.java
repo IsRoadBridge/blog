05ee0381c45a2989c58c710b7cd33d03ac5e4be5
IsRoadBridge <2849402866@qq.com>
2020-05-08 13:47:11 +0800

    开发雏形

---
+++ src/main/java/com/zxq/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.zxq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/index")
+    public  String index(){
+        return "index";
+    }
+
+    @GetMapping("/_fragments")
+    public  String fragments(){
+        return "_fragments";
+    }
+
+    @GetMapping("/about")
+    public  String about(){
+        return "about";
+    }
+
+    @GetMapping("/archives")
+    public  String archives(){
+        return "archives";
+    }
+
+    @GetMapping("/blog")
+    public  String blog(){
+        return "blog";
+    }
+
+    @GetMapping("/search")
+    public  String search(){
+        return "search";
+    }
+
+    @GetMapping("/tags")
+    public  String tags(){
+        return "tags";
+    }
+    @GetMapping("/types")
+    public  String types(){
+        return "types";
+    }
+}
