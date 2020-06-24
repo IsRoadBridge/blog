4faecad6c608595c24f6f5e06cce24ed9f6de703
IsRoadBridge <2849402866@qq.com>
2020-06-24 17:27:25 +0800

    blog表添加一列tag_id

--- src/main/resources/blog.sql
+++ src/main/resources/blog.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50725
 File Encoding         : 65001
 
-Date: 2020-05-18 17:34:09
+Date: 2020-06-24 17:26:00
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS `t_blog`;
 CREATE TABLE `t_blog` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
-  `content` text,
+  `content` longtext,
   `first_picture` varchar(255) DEFAULT NULL,
   `flag` varchar(255) DEFAULT NULL,
   `views` int(11) DEFAULT NULL,
@@ -35,12 +35,14 @@ CREATE TABLE `t_blog` (
   `update_time` datetime DEFAULT NULL,
   `type_id` bigint(20) DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
+  `tag_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of t_blog
 -- ----------------------------
+INSERT INTO `t_blog` VALUES ('1', 'first', '# 一级标题\r\n## 二级标题\r\n### 三级标题\r\n#### 四级标题\r\n##### 五级标题\r\n###### 六级标题\r\n- 文本一\r\n- 文本二\r\n- 文本三\r\n1. 文本一\r\n2. 文本二\r\n3. 文本三\r\n> Markdown 是一种轻量级标记语言，它允许人们使用易读易写的纯文本格式编写文档，然后转换成格式丰富的HTML页面。\r\n// <pre>\r\n    ```\r\n     编程语言\r\n     这是代码段\r\n    ```\r\n//</pre>\r\n| 项目 | 价格 | 数量 |\r\n| :-------- | --------:| :--: |\r\n| iPhone | 6000 元 | 5 |\r\n| iPad | 3800 元 | 12 |\r\n| iMac | 10000 元 | 234 |', 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592992464377&di=41128db4e0509649c365828f330752ca&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F56%2F12%2F01300000164151121576126282411.jpg', '转载', null, '1', '1', '1', '1', '1', '2020-06-24 07:06:53', '2020-06-24 07:06:53', '7', null, '2,3');
 
 -- ----------------------------
 -- Table structure for t_blog_tags
@@ -51,7 +53,7 @@ CREATE TABLE `t_blog_tags` (
   `tag_id` bigint(20) DEFAULT NULL,
   `blog_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of t_blog_tags
@@ -72,7 +74,7 @@ CREATE TABLE `t_comment` (
   `parent_comment_id` bigint(20) DEFAULT NULL,
   `admincomment` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of t_comment
@@ -86,11 +88,15 @@ CREATE TABLE `t_tag` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of t_tag
 -- ----------------------------
+INSERT INTO `t_tag` VALUES ('1', 'css');
+INSERT INTO `t_tag` VALUES ('2', 'java');
+INSERT INTO `t_tag` VALUES ('3', 'html5');
+INSERT INTO `t_tag` VALUES ('15', 'springcloud');
 
 -- ----------------------------
 -- Table structure for t_type
@@ -100,11 +106,13 @@ CREATE TABLE `t_type` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of t_type
 -- ----------------------------
+INSERT INTO `t_type` VALUES ('7', 'css');
+INSERT INTO `t_type` VALUES ('11', 'springcloud');
 
 -- ----------------------------
 -- Table structure for t_user
@@ -126,3 +134,4 @@ CREATE TABLE `t_user` (
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
+INSERT INTO `t_user` VALUES ('1', 'zxq', 'Bridge', '123456', null, null, null, null, null);
